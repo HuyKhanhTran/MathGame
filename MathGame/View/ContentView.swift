63e09c7260b51517481b86beddd06f059e4b1df5
HuyKhanhTran <s3804620@rmit.edu.vn>
2023-08-28 13:00:48 +0700

    add sound

--- MathGame/View/ContentView.swift
+++ MathGame/View/ContentView.swift
@@ -21,15 +21,8 @@ struct ContentView: View {
 @available(iOS 15.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
             ContentView()
-                .previewDevice(PreviewDevice(rawValue: "iPad Pro (11-inch) (4th generation)"))
-                .previewDisplayName("iPad Pro 11-inch")
-
-            ContentView()
-                .previewDevice(PreviewDevice(rawValue: "iPad Pro (12.9-inch) (6th generation)"))
-                .previewDisplayName("iPad Pro 12.9-inch")
-        }
+        
     }
 }
 
